d38c88e7908f8c947db8957cb684073dcf94c826
Sebastian Froger <43187719+SebastianFroger@users.noreply.github.com>
2024-02-21 23:58:55 +0100

    first commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float planetRadius = 100f;
+    public float moveDistance = 1;
+
+    public void Start()
+    {
+        
+    }
+}
